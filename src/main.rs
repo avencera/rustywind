593224d591af280c6d35191314d1e7f93521f7a9
Praveen Perera <ppraveen25@gmail.com>
2019-11-23 14:36:53 -0500

    Clean up output logs

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,48 @@
 use ignore::WalkBuilder;
 use std::env;
 use std::fs;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let current_dir = env::current_dir().expect("Connot run in current directory");
 
-    let walker = WalkBuilder::new(current_dir)
+    let walker = WalkBuilder::new(&current_dir)
         .build()
         .filter_map(Result::ok)
         .filter_map(|f| if f.path().is_dir() { None } else { Some(f) });
 
     for file in walker {
-        let file_name = file.path();
+        let file_path = file.path();
 
-        match fs::read_to_string(file_name) {
+        match fs::read_to_string(file_path) {
             Ok(contents) => {
                 if rustywind::has_classes(&contents) {
                     let sorted_content = rustywind::sort_file_contents(contents);
 
-                    println!("\n\n--------------------------------------");
-                    println!("Classes detected!");
-                    println!("filename: {}\n", file_name.display());
-                    println!("saving file.....");
-
-                    match fs::write(file_name, sorted_content.as_bytes()) {
-                        Ok(_) => println!("file: ({}) has been saved!", file_name.display()),
+                    match fs::write(file_path, sorted_content.as_bytes()) {
+                        Ok(_) => println!(" * {}", get_file_name(file_path, &current_dir)),
                         Err(err) => {
-                            println!("Error: {:?}", err);
-                            println!("Unable to to save file: {}", file_name.display());
+                            println!("\nError: {:?}", err);
+                            println!(
+                                "Unable to to save file: {}",
+                                get_file_name(file_path, &current_dir)
+                            );
                         }
                     }
-                    println!("--------------------------------------\n\n");
-                } else {
-                    println!("--------------------------------------");
-                    println!("No classes found in:  {}!", file_name.display());
-                    println!("--------------------------------------");
                 }
             }
-            Err(error) => {
-                println!("--------------------------------------");
-                println!("Unable to read file: {}", file_name.display());
-                println!("error: {:?}", error);
-                println!("--------------------------------------\n");
-            }
+            Err(_error) => (),
         }
+    }
+}
+
+fn get_file_name(file_path: &Path, dir: &PathBuf) -> String {
+    match (file_path.to_str(), dir.to_str()) {
+        (Some(file_path_str), Some(dir_str)) => file_path_str
+            .replace(dir_str, "")
+            .replacen("/", "", 1)
+            .to_string(),
+
+        _ => file_path.display().to_string(),
     }
 }
