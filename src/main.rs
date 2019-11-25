76cbe6f524098a3a8be5abda7f7e7c3c8258a622
Praveen Perera <ppraveen25@gmail.com>
2019-11-25 12:12:33 -0500

    Use crate version as version for cli

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 
 fn main() {
     let matches = App::new("Rusty Wind")
-        .version("0.1.0")
+        .version(clap::crate_version!())
         .setting(AppSettings::ArgRequiredElseHelp)
         .author("Praveen Perera <praveen@avencera.com>")
         .about("Organize all your tailwind classes")
