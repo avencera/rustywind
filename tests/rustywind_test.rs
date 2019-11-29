a491a895e61d3912b1c5d64a0b27effc79370ad8
Praveen Perera <ppraveen25@gmail.com>
2019-11-29 08:41:21 -0500

    Update appveyor and travis build scripts

--- tests/rustywind_test.rs
+++ tests/rustywind_test.rs
@@ -29,7 +29,7 @@ fn test_sort_file_contents() {
 fn test_sort_file_contents_with_duplicates() {
     let file_contents = r#"
     <div>
-        <div class='absolute relative flex inline-block inline random-class justify-items another-random-class flex flex flex'>
+        <div class='absolute relative flex flex flex flex inline-block inline random-class justify-items another-random-class'>
             <ul class='flex items-center md:pr-4 lg:pr-6'>
         </div>
     </div>
@@ -53,7 +53,7 @@ fn test_sort_file_contents_with_duplicates() {
 fn test_does_not_remove_duplicates_if_bool_set() {
     let file_contents = r#"
     <div>
-        <div class='absolute relative flex inline-block inline random-class justify-items another-random-class flex flex flex'>
+        <div class='absolute relative flex flex flex flex inline-block inline random-class justify-items another-random-class'>
             <ul class='flex items-center md:pr-4 lg:pr-6'>
         </div>
     </div>
