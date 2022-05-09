139c40ecd45b2e8be2bcae287523e082a8ad06dd
Praveen Perera <praveen@avencera.com>
2022-05-09 18:46:04 -0400

    Fix install script after github change

--- install.sh
+++ install.sh
@@ -81,6 +81,7 @@ need install
 need mkdir
 need mktemp
 need tar
+need grep
 
 # Optional dependencies
 if [ -z "$crate" ] || [ -z "$tag" ] || [ -z "$target" ]; then
@@ -121,7 +122,7 @@ fi
 url="$url/releases"
 
 if [ -z "$tag" ]; then
-    tag=$(curl -s "$url/latest" | cut -d'"' -f2 | rev | cut -d'/' -f1 | rev)
+    tag=$(curl "https://api.github.com/repos/$git/releases/latest" | grep "tag_name" | cut -d'"' -f4)
     say_err "Tag: latest ($tag)"
 else
     say_err "Tag: $tag"
