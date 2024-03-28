f837304e999b58413ad708fc0b0490012089de99
Danik Vitek <x3665107@gmail.com>
2024-03-28 17:59:43 +0200

    Update defaults.rs
    - Add support for `#` in class names

--- src/defaults.rs
+++ src/defaults.rs
@@ -3,7 +3,7 @@ use once_cell::sync::Lazy;
 use regex::Regex;
 
 pub static RE: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r#"\b(?:class(?:Name)?\s*=\s*["'])([_a-zA-Z0-9\.,\s\-:\[\]()/]+)["']"#).unwrap()
+    Regex::new(r#"\b(?:class(?:Name)?\s*=\s*["'])([_a-zA-Z0-9\.,\s\-:\[\]()/#]+)["']"#).unwrap()
 });
 
 pub static SORTER: Lazy<HashMap<String, usize>> = Lazy::new(|| {
